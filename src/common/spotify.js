8eb1f734517b6655f475de1c124556e032746704
yashonomous <yash.soni4@gmail.com>
2022-09-18 20:35:34 +0530

    changed redirect uri

--- src/common/spotify.js
+++ src/common/spotify.js
@@ -19,7 +19,7 @@ const SCOPES = [
 export const LOGIN_URL = `${encodeURI(
   AUTH_END_POINT
 )}?client_id=${CLIENT_ID}&redirect_uri=${encodeURI(
-  REDIRECT_URI_LOCAL
+  REDIRECT_URI
 )}&scope=${encodeURI(SCOPES.join(" "))}&response_type=token&show_dialog=true`;
 
 export const getHeadersFromUrl = (url) => {
